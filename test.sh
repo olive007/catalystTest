db6a37cedc55a0e86b3a513bedb946df8bb52cc9
SECRET Olivier <olivier@devolive.be>
2018-05-12 11:02:06 +0200

    get data from file

--- test.sh
+++ test.sh
@@ -12,33 +12,55 @@ unitTestFailed() {
 ########
 echo "Testing user_upload.php script";
 
-# Check arguments
-#  multiple file
+# Checking arguments
 errorMsg=`php user_upload.php --file ersdsd --file qsdqsd 2>&1 >/dev/null`
-if [ "$errorMsg" != "Error: multiple filename specified" ] || [ $? != 1 ] 
+if [ $? != 1 ] || [ "$errorMsg" != "Error: multiple filename specified" ]
 then
 	unitTestFailed "Multiple file"
 fi
 
-#  multiple username
 errorMsg=`php user_upload.php -u root -u olive 2>&1 >/dev/null`
-if [ "$errorMsg" != "Error: multiple MySQL username specified" ] || [ $? != 1 ] 
+if [ $? != 1 ] || [ "$errorMsg" != "Error: multiple MySQL username specified" ]
 then
 	unitTestFailed "Multiple username"
 fi
 
-#  multiple password
 errorMsg=`php user_upload.php -p pass -p toto 2>&1 >/dev/null`
-if [ "$errorMsg" != "Error: multiple MySQL password specified" ] || [ $? != 1 ] 
+if [ $? != 1 ] || [ "$errorMsg" != "Error: multiple MySQL password specified" ]
 then
 	unitTestFailed "Multiple password"
 fi
 
-#  multiple hostname
 errorMsg=`php user_upload.php -h 127.0.0.1 -h mysqlhost.com 2>&1 >/dev/null`
-if [ "$errorMsg" != "Error: multiple MySQL hostname specified" ] || [ $? != 1 ] 
+if [ $? != 1 ] || [ "$errorMsg" != "Error: multiple MySQL hostname specified" ]
 then
 	unitTestFailed "Multiple hostname"
 fi
+
+# Check the file
+errorMsg=`php user_upload.php --file csvForTest/none.csv 2>&1 >/dev/null`
+if [ $? != 2 ] || [ "$errorMsg" != "Error: Can't open csvForTest/none.csv" ]
+then
+	unitTestFailed "file doesn't exist"
+fi
+
+errorMsg=`php user_upload.php --file csvForTest/empty.csv 2>&1 >/dev/null`
+if [ $? != 2 ] || [ "$errorMsg" != "Error: csvForTest/empty.csv doesn't have correct data" ]
+then
+	unitTestFailed "file empty"
+fi
+
+errorMsg=`php user_upload.php --file csvForTest/wrongHeader1.csv 2>&1 >/dev/null`
+if [ $? != 2 ] || [ "$errorMsg" != "Error: csvForTest/wrongHeader1.csv doesn't have correct data" ]
+then
+	unitTestFailed "file with wrong data"
+fi
+
+errorMsg=`php user_upload.php --file csvForTest/wrongHeader2.csv 2>&1 >/dev/null`
+if [ $? != 2 ] || [ "$errorMsg" != "Error: csvForTest/wrongHeader2.csv doesn't have correct data" ]
+then
+	unitTestFailed "file with wrong data"
+fi
+
 
 exit 0;
--- user_upload.php
+++ user_upload.php
@@ -3,6 +3,11 @@
 # Exit Code
 const SUCCESS_EXIT = 0;
 const ARGUMENT_ERROR_EXIT = 1;
+const FILE_ERROR_EXIT = 2;
+const EMAIL_ERROR_EXIT = 3;
+
+# Remove php error and warning message
+error_reporting(NULL);
 
 #######################
 # Argument definition #
@@ -28,7 +33,7 @@ $arguments = getopt($shortOptions, $longOptions);
 ########################
 # Initialize variables #
 ########################
-$filename = "user.csv"; # Default filename
+$filename = "users.csv"; # Default filename
 $alterTable = true;
 $mysql['username'] = "root";
 $mysql['password'] = "";
@@ -119,6 +124,79 @@ if (array_key_exists('h', $arguments)) {
 	# Change the filename
 	$mysql['hostname'] = $arguments['h'];
 }
+
+# Open the file in read only mode
+$fd = fopen($filename, "r");
+
+# Check the opening the file
+if (!$fd) {
+
+	printError("Can't open $filename");
+	exit(FILE_ERROR_EXIT);
+}
+
+$columnName = fgetcsv($fd);
+if (gettype($columnName) != 'array' || count($columnName) <= 3) {
+
+	printError("$filename doesn't have correct data");
+	exit(FILE_ERROR_EXIT);
+}
+
+for ($i = count($columnName); --$i >= 0;) {
+	$columnName[$i] = strtolower(trim($columnName[$i]));
+
+	if ($columnName[$i] == "name") {
+		$nameIndex = $i;
+	}
+
+	if ($columnName[$i] == "surname") {
+		$surnameIndex = $i;
+	}
+	if ($columnName[$i] == "email") {
+		$emailIndex = $i;
+	}
+}
+
+if (!isset($nameIndex) || !isset($surnameIndex) || !isset($emailIndex)) {
+	printError("$filename doesn't have correct data");
+	exit(FILE_ERROR_EXIT);
+}
+
+$row = 0;
+$data = [];
+$data['name'] = [];
+$data['surname'] = [];
+$data['email'] = [];
+
+$formatData = [];
+
+$formatData['name'] = function($name) {
+	return ucfirst(trim($name));
+};
+$formatData['surname'] = function($surname) {
+	return ucfirst(trim($surname));
+};
+$formatData['email'] = function($email) {
+	$email = strtolower(trim($email));
+
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		printError("this addr: '$email' is not correct\n");
+		exit(EMAIL_ERROR_EXIT);
+	}
+
+	return $email;
+};
+
+
+while (($buffer = fgetcsv($fd)) !== FALSE) {
+	array_push($data['name'],    $formatData['name']($buffer[$nameIndex]));
+	array_push($data['surname'], $formatData['surname']($buffer[$surnameIndex]));
+	array_push($data['email'],   $formatData['email']($buffer[$emailIndex]));
+}
+
+fclose($fd);
+
+var_dump($data);
 
 # Stop the script without error
 exit(SUCCESS_EXIT);
